96c12347ade20af3fa7198812eaf0696ad6580b7
Danilo Volles <danilo.volles@outlook.com>
2023-08-24 08:32:59 -0300

    GuestForm implementation

--- app/src/main/java/com/example/convidados/GestFormActivity.kt
+++ app/src/main/java/com/example/convidados/GestFormActivity.kt
@@ -2,10 +2,30 @@ package com.example.convidados
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import com.example.convidados.databinding.ActivityGestFormBinding
 
-class GestFormActivity : AppCompatActivity() {
+class GestFormActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityGestFormBinding
+    private lateinit var viewModel: GuestFormViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_gest_form)
+
+        binding = ActivityGestFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this)[GuestFormViewModel::class.java]
+
+        binding.buttonSave.setOnClickListener(this)
+        binding.radioPresent.isChecked = true
+    }
+
+    override fun onClick(view: View) {
+        when(view.id){
+            R.id.button_save -> TODO()
+        }
     }
 }
